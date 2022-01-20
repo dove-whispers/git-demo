b27a171f313ff0a751e8cf32de54c67cf0a2f690
dove_whispers <2209238480@qq.com>
2022-01-20 23:27:36 +0800

    first commit

---
+++ src/main/java/com/dove/servlet/LoginServlet.java
@@ -0,0 +1,30 @@
+package com.dove.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 登录
+ *
+ * @author dove_whispers
+ * @date 2021/11/15
+ */
+@WebServlet("/hello")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("启动成功,访问到初始页面...");
+        response.sendRedirect(request.getContextPath() + "/hello.html");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doPost(request, response);
+    }
+}
